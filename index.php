c4f9c52148868346ebf575a44c1312ebd34a96b4
Jorge Carrasco <jorge.carrasco@solucionapp.es>
2023-12-01 18:59:20 +0100

    update database_generator.txt

--- index.php
+++ index.php
@@ -9,20 +9,13 @@ echo $DB_PASSWORD . "<br>";
 echo $DB_NAME . "<br>";
 
 // Create connection
-//$conn = mysqli_connect($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME, $PORT)
-//   or die("Connection failed: " . mysqli_connect_error());
+$conn = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME)
+    or die("Connection failed: " . mysqli_connect_error());
 
-
-// Create a connection
-$conn = new mysqli($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME);
 
 // Check the connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+echo "Connected successfully";
