ace6b08f44b4bd49547db659552be2193c9a05cf
Rosa <administrador@MacBook-Air-de-Rosa.local>
2023-12-02 19:50:23 +0100

    añadir y eliminar inversores

---
+++ src/administrador/alta_inversor.php
@@ -0,0 +1,43 @@
+<?php
+
+
+session_start();
+
+require_once __DIR__ . '/../../config.php';
+
+//Conectar con la base de datos
+$conexion = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME)
+    or die("No se ha podido conectar con la base de datos");
+
+
+//Recoger los datos del formulario
+$nombre = $_POST['nombre'];
+$correo = $_POST['correo'];
+$telefono = $_POST['telefono'];
+
+
+//Generar un id para el inversor (numerico)
+$consulta = "SELECT MAX(idInversor) FROM Inversor";
+$resultado = mysqli_query($conexion, $consulta);
+$fila = mysqli_fetch_array($resultado);
+$idInversor = $fila[0] + 1;
+
+
+//Insertar los datos en la base de datos
+$consulta = "INSERT INTO Inversor (idInversor, nombreInversor, correo, tlf) VALUES ('$idInversor', '$nombre', '$correo', '$telefono')";
+$resultado = mysqli_query($conexion, $consulta);
+
+if ($resultado) {
+    echo "Se ha insertado correctamente";
+    redirectListadoInversores();
+} else {
+    echo "No se ha podido insertar";
+}
+
+//Redirigir a la página de listado de mentores
+function redirectListadoInversores()
+{
+    header("Location: listado_inversores.php");
+    exit;
+}
+
---
+++ src/administrador/listado_inversores.php
@@ -0,0 +1,44 @@
+<?php
+
+session_start();
+
+require_once __DIR__ . '/../../config.php';
+
+//Conectar con la base de datos
+$conexion = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME)
+    or die("No se ha podido conectar con la base de datos");
+
+
+//Seleccionar todos los mentores
+$consulta = "SELECT * FROM Inversor";
+$resultado = mysqli_query($conexion, $consulta);
+
+//Visualizar todos los inversores
+$numero_filas = mysqli_num_rows($resultado);
+$numero_columnas = mysqli_num_fields($resultado);
+
+echo "<h2>Inversores</h2>";
+echo "<table border='1'><tr>";
+for ($i = 0; $i < $numero_columnas; $i++) {
+    $nombreColumna = mysqli_fetch_field_direct($resultado, $i)->name;
+    echo "<th>$nombreColumna</th>";
+}
+echo "</tr>";
+for ($i = 0; $i < $numero_filas; $i++) {
+    $fila = mysqli_fetch_array($resultado);
+    echo "<tr>";
+    for ($j = 0; $j < $numero_columnas; $j++) {
+        echo "<td>" . $fila[$j] . "</td>";
+    }
+    echo "<td><form action='eliminar_nversor.php' method='post'><input type='hidden' name='idInversor' value='" . $fila[0] . "'><input type='submit' value='Eliminar'></form></td>";
+
+    echo "</tr>";
+}
+echo "</table>";
+
+function redirectEliminarInversor($idInversor)
+{
+    $_SESSION['idInversor'] = $idInversor;
+    header("Location: eliminar_inversor.php");
+    exit;
+}
