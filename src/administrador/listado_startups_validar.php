e4d89e6949f4937f8d6fd662e523b71b2876625d
Jorge Carrasco <jorge.carrasco@solucionapp.es>
2023-12-02 19:24:25 +0100

    working on listado startups

--- src/administrador/listado_startups_validar.php
+++ src/administrador/listado_startups_validar.php
@@ -8,8 +8,36 @@ require_once __DIR__ . '/../../config.php';
 $conexion = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME)
     or die("No se ha podido conectar con la base de datos");
 
-echo "Conexión con la base de datos establecida"; //TODO: remove
-
 //Seleccionar todas las startups que estén en estado pendiente
 $consulta = "SELECT * FROM Startup WHERE estado = 'pendiente'";
 $resultado = mysqli_query($conexion, $consulta);
+
+//Mostrar todas las startups que estén en estado pendiente
+$numero_filas = mysqli_num_rows($resultado);
+$numero_columnas = mysqli_num_fields($resultado);
+
+echo "<h2>Startups pendientes de validar</h2>";
+echo "<table border='1'><tr>";
+for ($i = 0; $i < $numero_columnas; $i++) {
+    $nombreColumna = mysqli_fetch_field_direct($resultado, $i)->name;
+    echo "<th>$nombreColumna</th>";
+}
+echo "</tr>";
+for ($i = 0; $i < $numero_filas; $i++) {
+    $fila = mysqli_fetch_array($resultado);
+    echo "<tr>";
+    for ($j = 0; $j < $numero_columnas; $j++) {
+        echo "<td>" . $fila[$j] . "</td>";
+    }
+    echo "<td><form action='validar_startup.php' method='post'><input type='hidden' name='idStartup' value='" . $fila[0] . "'><input type='submit' value='Validar'></form></td>";
+
+    echo "</tr>";
+}
+echo "</table>";
+
+function redirectValidarStartup($idStartup)
+{
+    $_SESSION['idStartup'] = $idStartup;
+    header("Location: validar_startup.php");
+    exit;
+}
