0685c05c92efe9fa3df6d47b174aea8dc20627f4
Jorge Carrasco <jorge.carrasco@solucionapp.es>
2023-12-04 21:32:43 +0100

    v1

---
+++ src/administrador/gestionar_eventos/inscribir_startup.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+require_once __DIR__ . '/../../../config.php';
+
+//Conectar con la base de datos
+$conexion = mysqli_connect($DB_SERVER, $DB_USER, $DB_PASSWORD, $DB_NAME)
+    or die("No se ha podido conectar con la base de datos");
+
+$idEvento = $_POST['idEvento'];
+$idStartup = $_POST['idStartup'];
+
+//Insertar los datos en la base de datos
+$consulta = "INSERT INTO Startup_Evento (idEvento, idStartup) VALUES ('$idEvento', '$idStartup')";
+$resultado = mysqli_query($conexion, $consulta);
+
+if ($resultado) {
+    header("Location: listado_eventos.php");
+} else {
+    echo "No se ha podido insertar";
+}
